ce1441553c711c5b9467a3dd6ae8a2980a57d759
Rolin Blake <rolinmblake@gmail.com>
2023-06-09 17:13:54 -0500

    removing setupProxy and changing front end axios.get to use vercel serverless function url and  /api/scrape serverless path

--- src/api/scrape.js
+++ src/api/scrape.js
@@ -1,17 +1,11 @@
-const firebase = require('firebase');
 const axios = require('axios');
-const cheerio = require('cheerio');
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   try {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     const response = await axios.get('https://bigcharts.marketwatch.com/quickchart/options.asp?symb=AAPL#');
-    const $ = cheerio.load(response.data);
-    const scrapedData = $('table.optionchain.shaded').text();
-    res.status(200).json({ data: scrapedData });
+    res.status(200).json({ data: response.data });
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'An error occurred' });
   }
-};
+}
