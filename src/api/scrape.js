ea21c0c3b025492ab8e4039e693264df6bc1e09b
Rolin Blake <rolinmblake@gmail.com>
2023-06-09 18:50:39 -0500

    https now http

--- src/api/scrape.js
+++ src/api/scrape.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 export default async function handler(req, res) {
   try {
-    const response = await axios.get('https://bigcharts.marketwatch.com/quickchart/options.asp?symb=AAPL', {
+    const response = await axios.get('http://bigcharts.marketwatch.com/quickchart/options.asp?symb=AAPL', {
       httpAgent: new https.Agent({ rejectUnauthorized: false }),
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
