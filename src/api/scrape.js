79f4bcf4e15e54b344dcc1e268c6ea150504bfce
Rolin Blake <rolinmblake@gmail.com>
2023-06-09 16:56:52 -0500

    remove setupProxy.js and associations

--- src/api/scrape.js
+++ src/api/scrape.js
@@ -4,8 +4,6 @@ const cheerio = require('cheerio');
 
 module.exports = async (req, res) => {
   try {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     const response = await axios.get('https://bigcharts.marketwatch.com/quickchart/options.asp?symb=AAPL#');
     const $ = cheerio.load(response.data);
     const scrapedData = $('table.optionchain.shaded').text();
