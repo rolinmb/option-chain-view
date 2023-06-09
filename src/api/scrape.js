1af9ac25a395e034c832801bfdf708ff940b8ff5
Rolin Blake <rolinmblake@gmail.com>
2023-06-09 18:39:33 -0500

    adding more headers to axios.get() scrape.js request

--- src/api/scrape.js
+++ src/api/scrape.js
@@ -3,13 +3,15 @@ const axios = require('axios');
 export default async function handler(req, res) {
   try {
     const response = await axios.get('https://bigcharts.marketwatch.com/quickchart/options.asp?symb=AAPL', {
+      httpAgent: new https.Agent({ rejectUnauthorized: false }),
       headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
       },
     });
     res.setHeader('Content-Type', 'text/html');
-    res.status(200).json({ data: response.data });
+    res.status(200).send(response.data);
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'An error occurred' });
