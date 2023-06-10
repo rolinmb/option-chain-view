737b6ae767f97a00f7e3be73175a8918f540dbb5
Rolin Blake <rolinmblake@gmail.com>
2023-06-10 00:56:00 -0500

    stopped trying to fetch and build .csv files on the fly

--- src/components/ChainPanel.js
+++ src/components/ChainPanel.js
@@ -1,32 +1,138 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 export class ChainPanel extends Component{
     constructor(props){
         super(props);
         this.state = {
-            chainData: null
+            chainData: []
         }
     }
 
-    componentDidMount() {
-        this.fetchData();
-    }
-
-    fetchData = async () => {
-        try {
-            const response = await axios.get('https://vercel-chain-scrape.vercel.app');
-            this.setState({ chainData: response.data });
-        } catch (error) {
-            console.error('Error:', error);
+    handleChainCsvSelect = (e) => {
+        const files = e.target.files || e.dataTransfer.files;
+        if(files.length > 0){
+            const file = files[0];
+            if(!file.name.includes('_chain.csv')){
+                alert('Please select a .csv file with name format: %TICKER%_chain.csv');
+                document.getElementById('chain_csv_select').value = '';
+                return;
+            }
+            const reader = new FileReader();
+            reader.readAsText(file);
+            reader.onload = (event) => {
+                const csvData = event.target.result;
+                const csvRows = csvData.split('\n');
+                var allData = [];
+                for(let r = 1; r < csvRows.length; r++){
+                    if(csvRows[r].split(',').length === 1 && csvRows[r].split(',')[0].trim() === ''){
+                        continue;
+                    }
+                    allData.push(csvRows[r]);
+                }
+                this.setState({
+                    chainData: allData
+                });
+            };
+        }else{
+            alert('Please select an option chain .csv file');
+            document.getElementById('chain_csv_select').value = '';
+            return;
         }
     };
 
     render(){
         return(
             <div id='chain_wrap'>
-                <h2>Response Status from Vercel Serverless Function</h2>
-                <p>{this.state.chainData}</p>
+                <div id='chain_input'>
+                    <h2>Selected Option Chain:</h2>
+                    <input id='chain_csv_select' type='file' accepts='.csv' onChange={this.handleChainCsvSelect} />
+                </div>
+                <div id='chain_display'>
+                    <table id='chain_table' border="1" style={{borderCollapse: 'collapse'}}>
+                        <thead>
+                            <tr>
+                                <th>Ticker</th>
+                                <th>Div. Yield</th>
+                                <th>Historical Volatility</th>
+                                <th>YTE</th>
+                                <th>Expiry</th>
+                                <th>Strike Price</th>
+                                <th>cLast</th>
+                                <th>cBid</th>
+                                <th>cMid</th>
+                                <th>cAsk</th>
+                                <th>cTheoVal</th>
+                                <th>cTheoDif</th>
+                                <th>cChange</th>
+                                <th>cVol</th>
+                                <th>cOpen Int.</th>
+                                <th>c&sigma;</th>
+                                <th>cMid &sigma;</th>
+                                <th>c&Delta;</th>
+                                <th>c&lambda;</th>
+                                <th>cVega</th>
+                                <th>c&rho;</th>
+                                <th>c&epsilon;</th>
+                                <th>c&Theta;</th>
+                                <th>c&Gamma;</th>
+                                <th>cVanna</th>
+                                <th>cCharm</th>
+                                <th>cVomma</th>
+                                <th>cVeta</th>
+                                <th>cSpeed</th>
+                                <th>cZomma</th>
+                                <th>cColor</th>
+                                <th>cUltima</th>
+                                <th>pLast</th>
+                                <th>pBid</th>
+                                <th>pMid</th>
+                                <th>pAsk</th>
+                                <th>pTheoVal</th>
+                                <th>pTheoDif</th>
+                                <th>pChange</th>
+                                <th>pVol</th>
+                                <th>pOpen Int.</th>
+                                <th>p&sigma;</th>
+                                <th>pMid &sigma;</th>
+                                <th>p&Delta;</th>
+                                <th>p&lambda;</th>
+                                <th>pVega</th>
+                                <th>p&rho;</th>
+                                <th>p&epsilon;</th>
+                                <th>p&Theta;</th>
+                                <th>p&Gamma;</th>
+                                <th>pVanna</th>
+                                <th>pCharm</th>
+                                <th>pVomma</th>
+                                <th>pVeta</th>
+                                <th>pSpeed</th>
+                                <th>pZomma</th>
+                                <th>pColor</th>
+                                <th>pUltima</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.chainData.map((row, rIndex) => {
+                                var cells = row.split(',');
+                                var tableCells = [];
+                                for(let cIndex = 0; cIndex < cells.length; cIndex++){
+                                    if(cIndex === 4){
+                                        tableCells.push(<td key={cIndex}>{cells[cIndex]+' '+cells[cIndex+1]}</td>);
+                                        cIndex++;
+                                        continue;
+                                    }else{
+                                        tableCells.push(<td key={cIndex}>{cells[cIndex]}</td>);
+                                    }
+                                }    
+                                return <tr key={rIndex}>{tableCells}</tr>;
+                            })}
+                        </tbody>
+                    </table>
+                    <h2>Selected Option Chain .csv raw:</h2>
+                    <ul id='chain_raw'>
+                        {this.state.chainData.map((elem, index) => (<li key={index}>{elem}</li>))}
+                    </ul>
+                </div>
             </div>
         );
     }
