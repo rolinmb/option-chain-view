a47bd43da903842f718be62b42bed5939607b0dc
Rolin Blake <rolinmblake@gmail.com>
2023-06-17 11:15:26 -0500

    added react-plotly.js back into the mix; only rendering one chart at a time; does not work on Firefox

--- src/components/ChainPanel.js
+++ src/components/ChainPanel.js
@@ -1,7 +1,21 @@
 import './ChainPanel.css'
 import React, { Component } from 'react';
+import { ChainSurface } from './ChainSurface.js';
 import { storage } from '../firebase';
 import { ref, getDownloadURL, listAll } from 'firebase/storage';
+
+var func_names = [
+    'TheoDif','IV','MidIV',
+    'Delta','Elasticity','Vega','Rho','Epsilon','Theta',
+    'Gamma','Vanna','Charm','Vomma','Veta','Speed',
+    'Zomma','Color','Ultima'
+];
+var func_strs = [
+    'TheoDif','&sigma;','Mid&sigma;',
+    '&Delta;','&lambda;','Vega','&rho;','&epsilon;','&Theta;',
+    '&Gamma;','Vanna','Charm','Vomma','Veta','Speed',
+    'Zomma','Color','Ultima'
+];
 
 export class ChainPanel extends Component{
     constructor(props){
@@ -9,7 +23,11 @@ export class ChainPanel extends Component{
         this.state = {
             ticker: '',
             chainData: [],
-            imgUrls: []
+            ytes: [],
+            strikes_yte: [],
+            call_df: {},
+            put_df: {},
+            pngUrls: []
         }
     }
 
@@ -21,7 +39,7 @@ export class ChainPanel extends Component{
                 listResult.items.map((item) => getDownloadURL(item)));
             const relevantUrls = allUrls.filter(str => str.includes(this.state.ticker));
             if(relevantUrls.length === 0){
-                alert('No .png files available for '+this.state.ticker+' option chain');
+                alert('No .png files available for '+this.state.ticker+' in Firebase Cloud Storage bucket');
             }else if(relevantUrls.length < 18){
                 alert('Not all possible .png files have been uploaded to Firebase Cloud Storage for '+this.state.ticker+'; only displaying what was found');
             }
@@ -43,6 +61,49 @@ export class ChainPanel extends Component{
     };
 
     handleChainCsvSelect = (e) => {
+        var uniqueYtes = [];
+        var strikesYte = []; // list of lists
+        var call_df = { // each key-value is a list of lists
+            theo_dif: [],
+            iv: [],
+            mid_iv: [],
+            delta: [],
+            lambda: [],
+            vega: [],
+            rho: [],
+            epsilon: [],
+            theta: [],
+            gamma: [],
+            vanna: [],
+            charm: [],
+            vomma: [],
+            veta: [],
+            speed: [],
+            zomma: [],
+            color: [],
+            ultima: []
+        };
+        var put_df = { // each key-value is a list of lists
+            theo_dif: [],
+            iv: [],
+            mid_iv: [],
+            delta: [],
+            lambda: [],
+            vega: [],
+            rho: [],
+            epsilon: [],
+            theta: [],
+            gamma: [],
+            vanna: [],
+            charm: [],
+            vomma: [],
+            veta: [],
+            speed: [],
+            zomma: [],
+            color: [],
+            ultima: []
+        };
+        var expNum = 0;
         const files = e.target.files || e.dataTransfer.files;
         if(files.length > 0){
             const file = files[0];
@@ -55,17 +116,70 @@ export class ChainPanel extends Component{
             const reader = new FileReader();
             reader.readAsText(file);
             reader.onload = (event) => {
+                var ticker = file.name.substring(0, file.name.indexOf('_'));
                 const csvData = event.target.result;
                 const csvRows = csvData.split('\n');
                 var allData = [];
                 for(let r = 1; r < csvRows.length; r++){
-                    if(csvRows[r].split(',').length === 1 && csvRows[r].split(',')[0].trim() === ''){
+                    var colData = csvRows[r].split(',');
+                    if(colData.length === 1 && colData[0].trim() === ''){
                         continue;
                     }
                     allData.push(csvRows[r]);
+                    if(!uniqueYtes.includes(colData[3])){
+                        expNum++;
+                        uniqueYtes.push(colData[3]);
+                        strikesYte.push([]);
+                        for(let key in call_df){
+                            call_df[key].push([]);
+                            put_df[key].push([]);
+                        }
+                    }
+                    strikesYte[expNum-1].push(colData[6]);
+                    call_df['theo_dif'][expNum-1].push(colData[12]);
+                    call_df['iv'][expNum-1].push(colData[16]);
+                    call_df['mid_iv'][expNum-1].push(colData[17]);
+                    call_df['delta'][expNum-1].push(colData[18]);
+                    call_df['lambda'][expNum-1].push(colData[19]);
+                    call_df['vega'][expNum-1].push(colData[20]);
+                    call_df['rho'][expNum-1].push(colData[21]);
+                    call_df['epsilon'][expNum-1].push(colData[22]);
+                    call_df['theta'][expNum-1].push(colData[23]);
+                    call_df['gamma'][expNum-1].push(colData[24]);
+                    call_df['vanna'][expNum-1].push(colData[25]);
+                    call_df['charm'][expNum-1].push(colData[26]);
+                    call_df['vomma'][expNum-1].push(colData[27]);
+                    call_df['veta'][expNum-1].push(colData[28]);
+                    call_df['speed'][expNum-1].push(colData[29]);
+                    call_df['zomma'][expNum-1].push(colData[30]);
+                    call_df['color'][expNum-1].push(colData[31]);
+                    call_df['ultima'][expNum-1].push(colData[32]);
+                    put_df['theo_dif'][expNum-1].push(colData[38]);
+                    put_df['iv'][expNum-1].push(colData[42]);
+                    put_df['mid_iv'][expNum-1].push(colData[43]);
+                    put_df['delta'][expNum-1].push(colData[44]);
+                    put_df['lambda'][expNum-1].push(colData[45]);
+                    put_df['vega'][expNum-1].push(colData[46]);
+                    put_df['rho'][expNum-1].push(colData[47]);
+                    put_df['epsilon'][expNum-1].push(colData[48]);
+                    put_df['theta'][expNum-1].push(colData[49]);
+                    put_df['gamma'][expNum-1].push(colData[50]);
+                    put_df['vanna'][expNum-1].push(colData[51]);
+                    put_df['charm'][expNum-1].push(colData[52]);
+                    put_df['vomma'][expNum-1].push(colData[53]);
+                    put_df['veta'][expNum-1].push(colData[54]);
+                    put_df['speed'][expNum-1].push(colData[55]);
+                    put_df['zomma'][expNum-1].push(colData[56]);
+                    put_df['color'][expNum-1].push(colData[57]);
+                    put_df['ultima'][expNum-1].push(colData[58]);
                 }
                 this.setState({
-                    chainData: allData
+                    ticker: ticker,
+                    chainData: allData,
+                    ytes: uniqueYtes,
+                    strikes_yte: strikesYte,
+                    call_df: call_df,
+                    put_df: put_df
                 });
             };
         }else{
@@ -74,19 +188,19 @@ export class ChainPanel extends Component{
             return;
         }
         // Update 'surfaces_area' <div> from Firebase Cloud Storage matching files
-        let surfacesArea = document.getElementById('surfaces_area');
-        if(!surfacesArea.hasChildNodes()){
-            this.fetchSurfaceImages(surfacesArea);
+        let imgsArea = document.getElementById('imgs_area');
+        if(!imgsArea.hasChildNodes()){
+            this.fetchSurfaceImages(imgsArea);
         }else{
-            while(surfacesArea.firstChild){
-                surfacesArea.removeChild(surfacesArea.firstChild);
+            while(imgsArea.firstChild){
+                imgsArea.removeChild(imgsArea.firstChild);
             }
-            this.fetchSurfaceImages(surfacesArea);
+            this.fetchSurfaceImages(imgsArea);
         }
     };
     
     render(){
-        const { chainData } = this.state;
+        const { chainData, ticker, ytes, strikes_yte, call_df, put_df } = this.state;
 
         return(
             <div id='chain_wrap' style={{ paddingTop: '20px' }}>
@@ -180,8 +294,30 @@ export class ChainPanel extends Component{
                         </tbody>
                     </table>
                 </div>
-                <h2>Selected Option Chain Surfaces:</h2>
-                <div id='surfaces_area'
+                <h2>Option Chain Surface from Plotly.js:</h2>
+                <select id="surface_select" onSelect={() => {}}>
+                    {func_names.map(method => {
+                        return <option key={method}>{method}</option>
+                    })}
+                </select>
+                <div id="csurface_area">
+                    <ChainSurface
+                        strikes={strikes_yte}
+                        ytes={ytes}
+                        data={call_df['iv']}
+                        title={ticker+" Call iv"}
+                    />
+                </div>
+                <div id="psurface_area">
+                    <ChainSurface
+                        strikes={strikes_yte}
+                        ytes={ytes}
+                        data={put_df['iv']}
+                        title={ticker+" Put iv"}
+                    />
+                </div>
+                <h2>Selected Option Chain Surface Images from Python:</h2>
+                <div id='imgs_area'
                     style={{
                         margin: 'auto !important', display: 'flex !important', flexDirection: 'column !important',
                         alignItems:'center !important', height: '300px !important'
---
+++ src/components/ChainSurface.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Plot from 'react-plotly.js';
+
+export class ChainSurface extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div id="plotly_area">
+                <Plot 
+                    data = {[{
+                        type: 'surface',
+                        x: this.props.strikes,
+                        y: this.props.ytes,
+                        z: this.props.data,
+                    }]}
+                    layout={{
+                        autosize: false,
+                        width: 750,
+                        height: 750,
+                        title: this.props.title
+                    }}
+                />
+            </div>
+        );
+    }
+}
